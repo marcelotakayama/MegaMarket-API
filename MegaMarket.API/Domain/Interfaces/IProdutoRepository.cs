99fa9266d25e90df4297ff3563a3da5651581884
marcelotakayama <marcelo.takayama00@gmail.com>
2025-03-28 15:24:12 -0300

    Finish DB configuration and initial migration

---
+++ MegaMarket.API/Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using MegaMarket.Domain.Entities;
+
+namespace MegaMarket.Domain.Interfaces
+{
+    public interface IProdutoRepository
+    {
+        Task<Produto> GetByIdAsync(int id);
+        Task<IEnumerable<Produto>> GetAllAsync();
+        Task AddAsync(Produto produto);
+        Task UpdateAsync(Produto produto);
+        Task DeleteAsync(int id);
+    }
+}
